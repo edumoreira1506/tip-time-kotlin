ca212112e3d73134be97cd664fdf8cec2db40255
Antonio Eduardo Moreira <amoreira@brex.com>
2024-06-22 11:01:19 -0300

    Add interactions to app

--- app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
@@ -5,10 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            onCalculateButtonClick()
+        }
+    }
+
+    private fun onCalculateButtonClick() {
+        val costOfServiceString = binding.costOfService.text.toString()
+        val costOfServiceDouble = costOfServiceString.toDouble()
+
+        val selectedId = binding.tipOption.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = costOfServiceDouble * tipPercentage
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
